980fb6b91e8c622f3dcfff0e45d10b80d69bdac4
nmautz <nmautz@zagmail.gonzaga.edu>
2023-11-04 11:28:53 -0700

    include filename in argv

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.static('public'));
 app.use(express.json());
 
 
-function generate_speech(text, voice){
-  const pythonProcess = spawn('python3', ['speech.py', text, voice]);
+function generate_speech(text, voice, filename){
+  const pythonProcess = spawn('python3', ['speech.py', text, voice, filename]);
 
   pythonProcess.stdout.on('data', (data) => {
     console.log(data.toString());
@@ -24,7 +24,6 @@ function generate_speech(text, voice){
 
 }
 
-
 app.post('/text_to_speech', (req, res) => {
   const { text, voicenum } = req.body; // Extract data from the JSON body
 
@@ -32,7 +31,7 @@ app.post('/text_to_speech', (req, res) => {
   console.log('Received text:', text);
   console.log('Selected voice:', voicenum);
 
-  generate_speech(text, voicenum)
+  generate_speech(text, voicenum,"audio.mp3")
 });
 
 app.listen(port, () => {
--- speech.py
+++ speech.py
@@ -5,5 +5,6 @@ import sys
 
 argv1 = sys.argv[1]
 argv2 = sys.argv[2]
+argv3 = sys.argv[3]
 
-print("INPUT: " + argv1 + ", " + argv2)
+print("INPUT: " + argv1 + ", " + argv2 + ", " + argv3)
