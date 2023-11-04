c502bb921e83130f35d64e4c9cf233f8b9c938ad
nmautz <nmautz@zagmail.gonzaga.edu>
2023-11-04 10:57:05 -0700

    public folder

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => {
   res.send('Hello, World!');
 });
 
